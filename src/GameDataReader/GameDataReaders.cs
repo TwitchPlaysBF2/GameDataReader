edb77e0d2c5ae29791c711fa3bc901b5688cd4b2
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-07-29 02:55:01 +0200

    Fix singleton not being singleton

--- src/GameDataReader/GameDataReaders.cs
+++ src/GameDataReader/GameDataReaders.cs
@@ -4,7 +4,7 @@ namespace GameDataReader;
 
 public static class GameDataReaders
 {
-    private static Lazy<IBf2DataReader> Bf2Singleton => new(() => new Bf2DataReader());
+    private static readonly Lazy<IBf2DataReader> Bf2Singleton = new(() => new Bf2DataReader());
 
     public static IBf2DataReader Bf2 => Bf2Singleton.Value;
 }
