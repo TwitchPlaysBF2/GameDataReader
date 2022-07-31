c1b7d5f91d9cf660b6ae18bf6a1b96af8e4b8979
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-08-01 01:58:05 +0200

    Refactor GameName by extracting to base class

--- src/GameDataReader/BattlefieldRefractorV1Common/Files/GlobalRefractorV1ConfigFile.cs
+++ src/GameDataReader/BattlefieldRefractorV1Common/Files/GlobalRefractorV1ConfigFile.cs
@@ -4,18 +4,18 @@ namespace GameDataReader.BattlefieldRefractorV1Common.Files;
 
 internal class GlobalRefractorV1ConfigFile : ConfigFile<GlobalRefractorV1ConfigFile>
 {
-    private readonly string _gameName, _modName;
+    private readonly string _modName;
 
     public GlobalRefractorV1ConfigFile(string gameName, string modName)
+        : base(gameName)
     {
-        _gameName = gameName;
         _modName = modName;
     }
-    
+
     protected override string GetFilePath()
     {
         var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        return $@"{appData}\VirtualStore\Program Files (x86)\EA GAMES\{_gameName}\Mods\{_modName}\Settings\Profile.con";
+        return $@"{appData}\VirtualStore\Program Files (x86)\EA GAMES\{GameName}\Mods\{_modName}\Settings\Profile.con";
     }
 
     public string GetCurrentlyActiveProfileName()
--- src/GameDataReader/BattlefieldRefractorV2Common/Files/ProfileRefractorV2ConfigFile.cs
+++ src/GameDataReader/BattlefieldRefractorV2Common/Files/ProfileRefractorV2ConfigFile.cs
@@ -4,11 +4,11 @@ namespace GameDataReader.BattlefieldRefractorV2Common.Files;
 
 internal class ProfileRefractorV2ConfigFile : ConfigFile<ProfileRefractorV2ConfigFile>
 {
-    private readonly string _gameName, _profileNumber, _nameSettingKey;
+    private readonly string _profileNumber, _nameSettingKey;
 
     public ProfileRefractorV2ConfigFile(string gameName, string profileNumber, string nameSettingKey)
+        : base(gameName)
     {
-        _gameName = gameName;
         _profileNumber = profileNumber;
         _nameSettingKey = nameSettingKey;
     }
@@ -16,7 +16,7 @@ internal class ProfileRefractorV2ConfigFile : ConfigFile<ProfileRefractorV2Confi
     protected override string GetFilePath()
     {
         var userDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        return $@"{userDocuments}\{_gameName}\Profiles\{_profileNumber}\Profile.con";
+        return $@"{userDocuments}\{GameName}\Profiles\{_profileNumber}\Profile.con";
     }
 
     public string GetPlayerName()
