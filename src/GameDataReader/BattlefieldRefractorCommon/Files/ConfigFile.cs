b7e64a0d4ba2c9ac7322c46cec8cb570d6b051f8
cetteup <me@cetteup.com>
2022-08-01 22:02:35 +0200

    refactor: Simplify Refractor config file by extending LineBasedConfigFile

--- src/GameDataReader/BattlefieldRefractorCommon/Files/ConfigFile.cs
+++ src/GameDataReader/BattlefieldRefractorCommon/Files/ConfigFile.cs
@@ -1,5 +1,4 @@
-﻿using GameDataReader.BattlefieldRefractorCommon.Parsing;
-using GameDataReader.Common.Files;
+﻿using GameDataReader.Common.Files;
 
 namespace GameDataReader.BattlefieldRefractorCommon.Files;
 
@@ -7,7 +6,7 @@ namespace GameDataReader.BattlefieldRefractorCommon.Files;
 /// Represents a Refractor engine .con configuration file.
 /// </summary>
 /// <typeparam name="T">The type of the config file.</typeparam>
-internal abstract class ConfigFile<T> : ConfigFile
+internal abstract class ConfigFile<T> : LineBasedConfigFile<T>
 {
     protected readonly string GameName;
 
@@ -16,24 +15,8 @@ internal abstract class ConfigFile<T> : ConfigFile
         GameName = gameName;
     }
 
-    protected override string GetSettingValue(string settingKey)
+    protected override string GetParsePattern()
     {
-        var settingFinder = ReadConfigFile();
-        var setting = settingFinder.GetSetting(settingKey);
-        var value = setting.ParseValue();
-        return value;
-    }
-
-    protected override Common.Parsing.SettingResolver ReadConfigFile()
-    {
-        var filePath = GetFilePath();
-        if (!File.Exists(filePath))
-            throw new FileNotFoundException(
-                "Couldn't find configuration data. Is the game installed?\r\n" +
-                $"{typeof(T).FullName} not found at location: {filePath}");
-
-        var configLines = File.ReadAllLines(filePath);
-        var resolver = new SettingResolver(configLines);
-        return resolver;
+        return "^(?<key>.*?) \"?(?<value>.*?)\"?$";
     }
 }
